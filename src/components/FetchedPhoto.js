c90253ae1d8ca8fc1a541b125c61e8557874dfdc
almazongit <ialmaz2001@gmail.com>
2024-05-29 00:16:55 +0500

    got video tag

--- src/components/FetchedPhoto.js
+++ src/components/FetchedPhoto.js
@@ -3,14 +3,14 @@ import 'react-lazy-load-image-component/src/effects/blur.css';
 
  export const FetchedPhoto = ({data}) => {
     // Проверяем, есть ли объект photo и не является ли он пустым
-    if (!data) {
-        return <div>Ошибка в получении данных</div>;
+    if (!data || !data.url) {
+        return <div>Ошибка в получении данных, возможно они еще не загружены.</div>;
     }
 
     return (
             <div className="fetched-photo">
                 <div className="photo-text">
-                    <img alt={data.title} src={data.url} /> 
+                    {data.url.includes('youtube.com') ? <iframe width="640" height="480" src={data.url} allowfullscreen /> : <img alt={data.title} src={data.url} /> }
                     <div className="text">
                         <h3>{data.title}</h3>
                         <p>{data.explanation}</p>
